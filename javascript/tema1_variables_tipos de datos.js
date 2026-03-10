88a6f00e4492ac5a904e80b1427b97e22620af6e
alejo.canda777@gmail.com <alejo.canda777@gmail.com>
2026-03-10 18:03:31 -0500

    tema 1 variables y tipos de datos

---
+++ javascript/tema1_variables_tipos de datos.js
@@ -0,0 +1,31 @@
+// El nombre del local no va a cambiar (const)
+const nombreCafeteria = "El Grano Feliz";
+
+// El nombre en el vaso del ticket actual (texto/string)
+let cliente = "Ana";
+
+// Lo que tiene que pagar (número/number)
+let totalAPagar = 5.5;
+
+// ¿Ya se le entregó su café? (booleano/boolean)
+let entregado = false;
+
+// 5 minutos después, Ana recibe su café:
+entregado = true;
+
+//Desafío Práctico: El Ticket de Compra
+//Eres el programador del nuevo sistema táctil para los baristas.
+//Tu Misión: Escribe el código en JavaScript (mentalmente o en un papel) para crear un nuevo "ticket" de un cliente llamado "Roberto" que compró 3 capuchinos. Debes declarar:
+//Una variable para el nombre del cliente (String).
+//Una variable para la cantidad de cafés (Number).
+//Una variable para saber si el pedido es "Para Llevar" (Boolean).
+//(Tómate unos segundos para pensarlo antes de ver la solución)
+
+// Usamos const porque en este ticket el nombre no cambiará
+const nombreCliente = "Roberto";
+
+// Usamos let por si Roberto decide agregar otro café a última hora
+let cantidadCafes = 3;
+
+// Es verdadero, Roberto se lleva su pedido
+let paraLlevar = true;
